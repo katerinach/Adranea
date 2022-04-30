8e9deb464e410fbbce5173cb9536076dfc7f3708
katerinach <33007374+katerinach@users.noreply.github.com>
2022-04-30 20:27:02 +0300

    Bug Fixes and wasd movement

--- Player/Stede.gd
+++ Player/Stede.gd
@@ -14,8 +14,8 @@ onready var animationState = animationTree.get("parameters/playback")
 func _physics_process(delta):
 	#MOVEMENT
 	var input_vector = Vector2.ZERO
-	input_vector.x = Input.get_action_strength("ui_right") - Input.get_action_strength("ui_left")
-	input_vector.y = Input.get_action_strength("ui_down") - Input.get_action_strength("ui_up")
+	input_vector.x = Input.get_action_strength("press_d") - Input.get_action_strength("press_a")
+	input_vector.y = Input.get_action_strength("press_s") - Input.get_action_strength("press_w")
 	input_vector = input_vector.normalized()
 	#CHARACTER ANIMATION TURNING IN CORRECT DIRECTIONS
 	
